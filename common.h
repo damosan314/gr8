eab62fc62c58577ff774d5b2c30bb56cc976b9e5
damosan314 <damosan@gmail.com>
2022-05-24 06:14:17 -0400

    Create common.h

---
+++ common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+typedef unsigned int  word;
+typedef unsigned int  u16;
+typedef int           s16;
+typedef unsigned char byte;
+typedef unsigned char u8;
+typedef signed char   sbyte;
+typedef signed char   s8;
+
+#endif
